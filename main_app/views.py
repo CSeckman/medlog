0ff4e9afe95743f79141bae5e2e84d322d69c6d5
C_Seckman <Cassie.seckman@gmail.com>
2021-12-30 18:11:33 -0500

    nav bar

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('Welcome to Med Log')
+  return render(request, 'about.html')
 
 def about(request):
   return render(request, 'about.html')
