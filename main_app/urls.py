67753461a57b2c18371e3d15abde2bbf787632ea
C_Seckman <Cassie.seckman@gmail.com>
2021-12-30 18:32:03 -0500

    hard coded meds render to med index template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('home/', views.about, name='about'),
+  path('medications/', views.med_index, name='med_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Med:
+  def __init__(self, name, num_of_doses, dose, other_details):
+    self.name = name
+    self.num_of_doses = num_of_doses
+    self.dose = dose
+    self.other_details = other_details
+
+meds = [
+  Med('Amoxicilin', '20', '5ml, 2 times a day', 'Take with food.'),
+  Med('Tylenol', '10', '5ml, 4 times a day', 'No Details'),
+  Med('Incilin', '...', '1ml, 3 times a day', 'Inject into thigh'),
+  
+]
+
+def med_index(request):
+  return render(request, 'meds/index.html', { 'meds': meds })
