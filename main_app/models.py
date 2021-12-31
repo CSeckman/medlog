9b33af0aabf041f76356d96128c904cf90336a4a
C_Seckman <Cassie.seckman@gmail.com>
2021-12-30 22:26:40 -0500

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Med(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    num_of_doses = models.IntegerField
+    dose = models.CharField(max_length=100)
+    other_details = models.CharField(max_length=150)
--- main_app/views.py
+++ main_app/views.py
@@ -8,19 +8,5 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Med:
-  def __init__(self, name, description,  num_of_doses, dose, other_details):
-    self.name = name
-    self.description = description
-    self.num_of_doses = num_of_doses
-    self.dose = dose
-    self.other_details = other_details
-
-meds = [
-  Med('Amoxicilin', 'for ear infection', '20', '5ml, 2 times a day', 'Take with food.'),
-  Med('Tylenol', 'to ease booster symptoms',  '10', '5ml, 4 times a day', 'No Details'),
-  Med('Incilin', 'Diabetes', '...', '1ml, 3 times a day', 'Inject into thigh'),
-]
-
 def med_index(request):
-  return render(request, 'meds/index.html', { 'meds': meds })
+  return render(request, 'meds/index.html')
