cbafec476167a9d89ceaa5faf84409d9914c5174
C_Seckman <Cassie.seckman@gmail.com>
2022-01-02 18:49:13 -0500

    styling and add bootstrap

--- main_app/models.py
+++ main_app/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Med(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
-    num_of_doses = models.IntegerField
-    dose = models.CharField(max_length=100)
-    other_details = models.CharField(max_length=150)
+  name = models.CharField(max_length=50)
+  description = models.TextField(max_length=200)
+  total = models.IntegerField(blank=True, null=True)
+  dose = models.CharField(max_length=100)
+  other_details = models.CharField(max_length=150)
+  
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
+
+from .models import Med
 
 def home(request):
   return render(request, 'home.html')
@@ -9,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def med_index(request):
-  return render(request, 'meds/index.html')
+  meds = Med.objects.all()
+  return render(request, 'meds/index.html', {"meds": meds})
