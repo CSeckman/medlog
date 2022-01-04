3ee7ff781690f88c0a285e54e51d30e202ba1644
C_Seckman <Cassie.seckman@gmail.com>
2022-01-03 20:38:04 -0500

    signup form

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path('meds/create/', views.MedCreate.as_view(), name='meds_create'),
   path('meds/<int:pk>/update/', views.MedUpdate.as_view(), name='med_update'),
   path('meds/<int:pk>/delete/', views.MedDelete.as_view(), name='med_delete'),
-  path('meds/<int:med_id>/log_dose', views.log_dose, name='log_dose')
+  path('meds/<int:med_id>/log_dose', views.log_dose, name='log_dose'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
 
@@ -25,8 +27,10 @@ def med_detail(request, med_id):
 class MedCreate(CreateView):
   model = Med
   fields = '__all__'
-  success_url = '/meds/'
   
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 class MedUpdate(UpdateView):
   model = Med
@@ -44,3 +48,17 @@ def log_dose(request, med_id):
     new_log.med_id = med_id
     new_log.save()
   return redirect('med_detail', med_id=med_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
